923dc06a9e652e88d072772f2614de764463ce27
pythondjango-project <ashu21692@gmail.com>
2023-11-07 21:04:23 +0530

    AP|folder change

--- web_data/views.py
+++ web_data/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 
+
 # Create your views here.
 '''class WebData_list(generics.ListCreateAPIView):
     queryset = WebData.objects.all()
